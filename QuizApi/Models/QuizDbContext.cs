f39ec1b8218bf699f54d0e1f49f41aeb9ada69dd
ayman jarmoune <ayman.jarmoune@gmail.com>
2022-10-31 00:19:40 +0100

    changes

---
+++ QuizApi/Models/QuizDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace QuizApi.Models
+{
+    public class QuizDbContext:DbContext
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+    }
+}
