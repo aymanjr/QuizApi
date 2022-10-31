ec5d9cab1420df50895ed64e90d0d766812aebcd
ayman jarmoune <ayman.jarmoune@gmail.com>
2022-10-31 17:07:59 +0100

    finishing the api for questions

---
+++ QuizApi/Migrations/20221031152528_initial Create.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizApi.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    ParticipanttId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Score = table.Column<int>(type: "int", nullable: true),
+                    TimeTaken = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.ParticipanttId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    QnId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QnInWords = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    ImageName = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Option1 = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Option2 = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Option3 = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Option4 = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    An = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QnId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
